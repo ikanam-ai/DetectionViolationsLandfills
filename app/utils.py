2239b6f32a91bbbdd11d8ec53836e64ed57238c7
Rinat Sharafetdinov <rinat.sharafetdinov42@gmail.com>
2023-05-27 20:32:38 +0300

    app

--- app/utils.py
+++ app/utils.py
@@ -1,15 +1,6 @@
 import os
 import streamlit as st
 
-
-st.set_config(
-    **{
-        "server": {
-            "baseUrl": "/tothelimbo-lct-2023-appmain-15s7vm"
-        }
-    }
-)
-
 def get_subdirs(b='.'):
     '''
         Returns all sub-directories in a specific Path
@@ -26,7 +17,7 @@ def get_detection_folder():
     '''
         Returns the latest folder in runs/detect
     '''
-    detection_folder = os.path.join(st.config.get_option("server.baseUrl"), "runs", "detect")
+    detection_folder = os.path.join(st.config.get_option("/tothelimbo-lct-2023-appmain-15s7vm"), "runs", "detect")
     subdirs = [d for d in os.listdir(detection_folder) if os.path.isdir(os.path.join(detection_folder, d))]
     latest_folder = max(subdirs, key=lambda x: os.path.getmtime(os.path.join(detection_folder, x)))
     return os.path.join(detection_folder, latest_folder)
