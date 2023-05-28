53183425b1a67f4a73d616bbeffb9483ca533769
Rinat Sharafetdinov <rinat.sharafetdinov42@gmail.com>
2023-05-28 23:48:22 +0300

    update

--- web/apps/yolo.py
+++ web/apps/yolo.py
@@ -76,7 +76,7 @@ def app():
     if is_valid:
         if st.button('Сделать предсказание'):
             with st.spinner('Подождите...'):
-                process = subprocess.run(['yolo', 'task=detect', 'mode=predict', 'model=models/model.pt', 'conf=0.08', 'source={}'.format(source)], capture_output=True, universal_newlines=True)
+                process = subprocess.run(['yolo', 'task=detect', 'mode=predict', 'model=models/best.pt', 'conf=0.15', 'source={}'.format(source)], capture_output=True, universal_newlines=True)
                 output = process.stderr
                 st.text(output)
 
